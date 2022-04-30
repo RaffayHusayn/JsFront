64c884a9a8f9e07ef8ceda1c873ccfa3773898e5
Raffay Syed <raffayhusayn@gmail.com>
2022-04-30 02:51:41 -0400

    Add some remaining changes, nothing major

--- Calc/calc.js
+++ Calc/calc.js
@@ -1,2 +1,7 @@
 document.getElementById("num1").textContent = 2;
 document.getElementById("num2").textContent = 4;
+
+
+ipc-button ipc-button--single-padding ipc-button--center-align-content ipc-button--default-height ipc-button--core-baseAlt ipc-button--theme-baseAlt ipc-button--on-onBase ipc-secondary-buttonlLink__MediaLinkButton-sc-yyqs5y-3.cVnyJL
+
+
