5a179fa5f3847f9ed6f6036f06eb0e5a3cbbd23c
Raffay Syed <raffayhusayn@gmail.com>
2022-05-01 23:44:26 -0400

    Created an HTML layout of the calculator

--- Calc/calc.js
+++ Calc/calc.js
@@ -1,7 +1,5 @@
-document.getElementById("num1").textContent = 2;
-document.getElementById("num2").textContent = 4;
+var tempNum = 0;
 
-
-ipc-button ipc-button--single-padding ipc-button--center-align-content ipc-button--default-height ipc-button--core-baseAlt ipc-button--theme-baseAlt ipc-button--on-onBase ipc-secondary-buttonlLink__MediaLinkButton-sc-yyqs5y-3.cVnyJL
-
-
+function add(){
+    
+}
