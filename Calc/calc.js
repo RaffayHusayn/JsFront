63ea704b399aba42626a96e1867e650ca831b67f
Raffay Syed <raffayhusayn@gmail.com>
2022-05-04 21:25:05 -0400

    Add code to properly display and remove text from the input box when operator is pressed

--- Calc/calc.js
+++ Calc/calc.js
@@ -1,11 +1,22 @@
-var tempCalc = 0;
-var tempString = "";
-
-function add(){
+var tempString = "" ;
+var operatorFlag = 0;
+function operation(operator){
+    operatorFlag++;
+    console.log("operatorFlag = "+operatorFlag);
 
 }
 
-function num(num){
-    tempString +=num;
-    document.getElementById("numtext").value= tempString;
+function numPressed(num){
+    if(operatorFlag === 1){
+        tempString = "";
+        document.getElementById("numtext").value = tempString;
+        tempString +=num;
+        document.getElementById("numtext").value = tempString;
+
+    }else{
+    tempString += num;
+    document.getElementById("numtext").value = tempString;
+
+    }
+
 }
