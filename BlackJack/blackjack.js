358749fe89771b599e9095918f5778c1d37eb8b3
Raffay Syed <raffayhusayn@gmail.com>
2022-05-07 19:49:07 -0400

    Add blackjack game logic in javascript

--- BlackJack/blackjack.js
+++ BlackJack/blackjack.js
@@ -1,4 +1,35 @@
-var console = document.getElementById("console")
+var messageEl = document.getElementById('message-el'); 
+var startGameBtn = document.getElementById('start-game');
+var sumEl = document.getElementById('sum-el');
+var cardEl = document.getElementById('card-el');
+
+var firstCard = Math.floor(Math.random() * 13);
+var secondCard = Math.floor(Math.random() * 13);
+var sum = firstCard + secondCard;
+var isAlive = true;
+var hasBlackjack = false;
+var message = "";
+
+ 
+
+
+
+
 function startGame(){
-    console.textContent = "Game Started"
+    sumEl.textContent = "Sum: " + sum;
+    // cardEl.textContent = 
+    if(sum <= 20){
+        message = "Do you want to play again?"
+        startGameBtn.textContent = "Play Again"
+
+    }else if(sum === 21){
+        message = "You have got Blackjack!!";
+        hasBlackjack = true;
+    }else{
+        message = "You are out of the game!!";
+        isAlive = false;
+    }
+
+    messageEl.textContent = message;
+
 }
