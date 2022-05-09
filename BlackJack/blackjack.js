638316f95364150b4581b986e302f0f9cb9c56ad
Raffay Syed <raffayhusayn@gmail.com>
2022-05-08 20:59:34 -0400

    Add new method to generate new card and a method to generate random numbers between a given range

--- BlackJack/blackjack.js
+++ BlackJack/blackjack.js
@@ -1,23 +1,36 @@
-var messageEl = document.getElementById('message-el'); 
-var startGameBtn = document.getElementById('start-game');
-var sumEl = document.getElementById('sum-el');
-var cardEl = document.getElementById('card-el');
+let messageEl = document.getElementById('message-el'); 
+let startGameBtn = document.getElementById('start-game');
+let sumEl = document.getElementById('sum-el');
+let cardEl = document.getElementById('card-el');
 
-var firstCard = Math.floor(Math.random() * 13);
-var secondCard = Math.floor(Math.random() * 13);
-var sum = firstCard + secondCard;
-var isAlive = true;
-var hasBlackjack = false;
-var message = "";
+// Single line arrow function
+const customRandomNumber = (min, max)=>(Math.floor(Math.random() * (max - min) + min));
 
- 
+/*
+Same Function written in different ways:
+1. Regular function:
+function customRandomNumber(min, max){
+    return (min, max)=>(Math.floor(Math.random() * (max - min) + min));
+}
+
+2. Arrow function without implicit return:
+const customRandomNumber = (min, max)=>{
+    return (min, max)=>(Math.floor(Math.random() * (max - min) + min));
+}
+*/
 
 
+let firstCard = customRandomNumber(2 , 11);
+let secondCard = customRandomNumber(2 , 11);
+let sum = firstCard + secondCard;
+let isAlive = true;
+let hasBlackjack = false;
+let message = "";
 
 
 function startGame(){
     sumEl.textContent = "Sum: " + sum;
-    // cardEl.textContent = 
+    cardEl.textContent = "Cards: " + firstCard + " " + secondCard;
     if(sum <= 20){
         message = "Do you want to play again?"
         startGameBtn.textContent = "Play Again"
@@ -32,4 +45,12 @@ function startGame(){
 
     messageEl.textContent = message;
 
+}
+
+function newCard(){
+    
+    let newCard = customRandomNumber(2 , 11);
+    console.log("New Card : " + newCard);
+    sum += newCard;
+    startGame();
 }
