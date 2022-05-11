b696d7c3dc2451e0656f5ad55ef487107cc77a2f
Raffay Syed <raffayhusayn@gmail.com>
2022-05-10 21:41:27 -0400

    No need to have an html to run js
    -- just have a <Script> tag with link to the js file is enough

--- Closure/lexicalScope.js
+++ Closure/lexicalScope.js
@@ -1,8 +1,19 @@
-for(var i = 0 ; i < 3 ; i++){
-    const log = ()=>{
+alert("code is starting");
+console.log("This prints 3 everytime because we are using Var which creates a variable and hoist it in the global scope");
+// for(var i = 0 ; i < 3 ; i++){
+//     const log = ()=>{
+//         console.log(i);
+//     }
+//     setTimeout(log, i*2000);
+// }
+
+
+console.log("This print 1 to 3 because this time we are using let which creates a variable whose scope is bounded by the for braces");
+for(let i =0 ; i<3 ; i++){
+    const log=()=>{
         console.log(i);
     }
-    setTimeout(log, 1000);
+    setTimeout(log, i*1000);
 }
 
 function outerFunc(){
@@ -15,3 +26,11 @@ function outerFunc(){
 
 console.log("Calling outer function which defines and call inner function");
 outerFunc();
+
+
+let nameArray = ["raffay", "ayesha", "nabia"];
+nameArray.forEach(name=>{
+    if(name.startsWith("r")){
+        console.log(name);
+    }
+});
