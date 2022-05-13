3c8f793343e326368f28af1c81cdee3bafbf6e67
Raffay Syed <raffayhusayn@gmail.com>
2022-05-13 00:00:00 -0400

    Add an incorrect and correct way to print 0 to 9 numbers with a 1 sec delay using closures

--- Closure/closureLoop.js
+++ Closure/closureLoop.js
@@ -1,22 +1,27 @@
-console.log("outside of for loop");
+/*
+example # 1 : Incorrect Closure Loop because there is no closure here and we will print 10 ten times
+*/
+console.log("Calling the incorrect Closure Loop");
 for(var i= 0 ; i < 10 ; i++){
-
-    console.log("inside the for loop");
     setTimeout(function(){
         console.log(i);
     }, i*1000);
 }
 
-function outside(){
-    for(var i = 0; i<10; i++){
-        function inside(i){
-            setTimeout(function(){
+/*
+example # 2: Correct Closure Loop because inside function creates a closure with the execution environment of the outside function which contains 
+the varible so this time we print 0 to 9 with 1 sec delay like intended
+*/
+
+function outside() {
+    for (var i = 0; i < 10; i++) {
+        function inside(i) {
+            setTimeout(function () {
                 console.log(i);
-            }, i*1000);
+            }, i * 1000);
         }
-        inside();
+        inside(i);
     }
 }
-
-console.log("calling the outside function nowwwwwww");
+console.log("Calling the correct Closure Loop");
 outside();
