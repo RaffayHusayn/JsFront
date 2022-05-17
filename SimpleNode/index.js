14e545b1ee4afb849cb4a2a647f057843f90b02a
Raffay Syed <raffayhusayn@gmail.com>
2022-05-16 23:28:27 -0400

    started working with Object Literals

--- SimpleNode/index.js
+++ SimpleNode/index.js
@@ -1,16 +1,41 @@
-class Person{
+// class Person{
 
-    constructor(name, age, height){
-        this.name = name;
-        this.age = age;
-        this.height = height;
-    }
+//     constructor(name, age, height){
+//         this.name = name;
+//         this.age = age;
+//         this.height = height;
+//     }
 
-    sing(){
-        this.name = "gay";
+//     sing(){
+//         this.name = "gay";
+//     }
+// }
+
+// let raffay = new Person("Raffay", 25, "5'10");
+// raffay.sing();
+// console.log(raffay);
+let fighter = {
+    name: "Ryan Hall",
+    weight_class: "light-weight",
+    record: "12-1-0",
+    nickname: "wizard",
+    move(move) {
+        console.log(this.name + " is doing the " + move);
+    },
+    move2(move) {
+        return () => {
+            console.log(this.name + " is doing the " + move);
+        };
+    },
+    move3(move){
+        console.log(`${this.name} is doing the third move `+move);
     }
 }
 
-let raffay = new Person("Raffay", 25, "5'10");
-raffay.sing();
-console.log(raffay);
+fighter.move("inamari roll");
+fighter.move2("gillotine")();
+fighter.move3("rear naked choke");
+
+
+
+// 18005544555 guest relations after 12 the manager is Dan
