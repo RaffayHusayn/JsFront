0c681b41ea93b71ca5d0440ef2c05126bc4e229e
Raffay Syed <raffayhusayn@gmail.com>
2022-05-27 20:36:02 -0400

    Add prototype classes

--- SimpleNode/prototypeChain.js
+++ SimpleNode/prototypeChain.js
@@ -15,3 +15,25 @@ function Employee(name, id){
 const raffay = new Employee("Raffay", 41);
 raffay.__proto__.eat();
 console.log(functionStoreEmployee);
+
+
+function Website(protocol){
+    this.protocol = protocol;
+    this.url = protocol + "://somewebsite.com";
+    this.printurl = function(){
+        console.log(this.url);
+    };
+};
+
+Website.prototype.printProtocol = function(){
+    console.log(this.protocol);
+}
+
+const unprotected = new Website("http");
+const protected = new Website("https");
+
+console.log(unprotected);
+console.log(protected);
+console.log("printing the protocol which is a function described using prototype property of the constructor function");
+unprotected.printProtocol();
+protected.printProtocol();
