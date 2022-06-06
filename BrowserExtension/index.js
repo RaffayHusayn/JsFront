921c38b7e1146df30d0fbc04d378419ea25e7186
Raffay Syed <raffayhusayn@gmail.com>
2022-06-06 02:58:11 -0400

    Add an Event listener on Input field so that Enter also work to press the button below the field

--- BrowserExtension/index.js
+++ BrowserExtension/index.js
@@ -6,22 +6,31 @@ let storeditems = [];
 
 buttonElement.addEventListener('click', ()=>{
        storeditems.push(inputElement.value); 
-       let li = document.createElement("li");
-       let a = document.createElement("a");
+       let liElement = document.createElement("li");
+       let aElement = document.createElement("a");
     //    a.setAttribute("href", "http://"+inputElement.value); 
     //    a.textContent = inputElement.value;
     //    a.target = "_blank";
-       Object.assign(a,{
+       Object.assign(aElement,{
            href : "http://" + inputElement.value,
            textContent : inputElement.value,
            target : "_blank"
        });
-       li.appendChild(a);
-       ulElement.appendChild(li);
+       liElement.appendChild(aElement);
+       ulElement.appendChild(liElement);
        inputElement.value = "";
        storeditems.forEach(item=>{
            console.log(item);
        })
     });
+
+inputElement.addEventListener("keyup",(event)=>{
+    event.preventDefault();
+    if(event.key==="Enter"){
+        buttonElement.click();
+    }
+
+
+})
 
 };
