995973cf8aea866f00f03bef41e197be0aedbb58
Raffay Syed <raffayhusayn@gmail.com>
2022-06-08 15:11:46 -0400

    Add code to understand Callbacks

--- LangSpecific/Async/Callback.js
+++ LangSpecific/Async/Callback.js
@@ -2,13 +2,22 @@ let objectList = [
     {name: "Raffay", age : 25},
     {name: "Ayesha", age : 23}
 ]
-function returnList(objList){
-    setTimeout(()=>{
-        objList.forEach(element => {
+function returnList(){
+    return setTimeout(()=>{
+        let output = '';
+        objectList.forEach(element => {
             let {name, age} = element;
-            document.body.innerHTML += `<li>${name} and the age is ${age}</li>`;
+            output += `<li>${name} and the age is ${age}</li>`;
         });
+        document.body.innerHTML = output;
     }, 2000);
 }
-returnList(objectList);
-// console.log(returnList(objectList));
+
+function addToList(newObj, callback){
+    setTimeout(()=>{
+        objectList.push(newObj);
+        callback();
+    }, 3000); 
+} 
+let newObject = {name:"Nabia", age: 14};
+addToList(newObject, returnList);
