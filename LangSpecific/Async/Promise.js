b2bdab33d654f1a143c6b7bc2f8a1231fc745c7a
Raffay Syed <raffayhusayn@gmail.com>
2022-06-09 19:03:12 -0400

    Adding code for Promises in js

--- LangSpecific/Async/Promise.js
+++ LangSpecific/Async/Promise.js
@@ -17,7 +17,6 @@ function addToList(newObj){
     let myPromise = new Promise((resolve, reject)=>{
         setTimeout(()=>{
             let error = true;
-            console.log("new object is not null: "+ newObj !=null);
             if(newObj != null){
                 error = false;
             }
@@ -27,10 +26,15 @@ function addToList(newObj){
             }else{
                 reject("Error: Something went wrong");
             }
+
+        console.log("inside of the settimeout in the addtolist function")
         }, 3000);
+        console.log("outside of the settime out in the addtolist function")
     });
     return myPromise;
-} 
+}
+console.log("outside of all the functions");
+
 let newObject = {name:"Nabia", age: 14};
 addToList(newObject)
 .then(returnList)
