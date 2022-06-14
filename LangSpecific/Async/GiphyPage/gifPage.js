6220290e1d2a450d01169132fa84a76f43954642
Raffay Syed <raffayhusayn@gmail.com>
2022-06-14 01:17:25 -0400

    Add code to Asynchronously call the function to get image Url and image Label using Promises

--- LangSpecific/Async/GiphyPage/gifPage.js
+++ LangSpecific/Async/GiphyPage/gifPage.js
@@ -14,12 +14,38 @@ function randomNumber(max){
 
 const max = gifKeys.length -1;
 
+function getImage(){
+    let imageObj = {};
+    let imagePromise = new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            let randomWord = gifKeys[randomNumber(max)];
+            let imageUrl = gifDictionary[randomWord];
+            console.log(randomWord);
+            console.log(gifDictionary[randomWord]);
+            if((randomWord != "") && imageUrl != ""){
+                imageObj.word = randomWord;
+                imageObj.image = gifDictionary[randomWord];
+                resolve(imageObj);
+            }else{
+                reject("there was an error");
+            }
+        }, 2000);
+    });
+    return imagePromise;
+}
+
 window.onload = function(){
     let body = document.body;
     let imageEl = document.getElementById("img-el");
     let headingEl = document.getElementById("heading-el");
-    console.log(imageEl);
-    let randomWord = gifKeys[randomNumber(max)];
-    headingEl.textContent = randomWord;
-    imageEl.src = gifDictionary[randomWord];
+
+
+
+    getImage()
+    .then((imageObj)=>{
+        headingEl.textContent = imageObj.word;
+        imageEl.src = imageObj.image;
+    })
+    .catch(err => console.log(err));
+    
 };
